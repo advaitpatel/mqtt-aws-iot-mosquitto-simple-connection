c869e9b9cdcfc33574412a6fccc2d3219bb5ff2d
Advait Patel <advait.patel@machfu.com>
2018-01-29 10:30:09 -0500

    Change this file with your own values

--- app/src/main/java/com/example/advai/mqttawsiotmosquitosimpleconnection/Constants.java
+++ app/src/main/java/com/example/advai/mqttawsiotmosquitosimpleconnection/Constants.java
@@ -11,11 +11,11 @@ public class Constants {
 
     // Customer specific IoT endpoint
     // AWS Iot CLI describe-endpoint call returns: XXXXXXXXXX.iot.<region>.amazonaws.com,
-    public static final String CUSTOMER_SPECIFIC_ENDPOINT = "a29ewwbs52vxv3.iot.us-east-1.amazonaws.com";
+    public static final String CUSTOMER_SPECIFIC_ENDPOINT = "XXXXXXXXXXXX.iot.us-east-1.amazonaws.com";
 
     // Cognito pool ID. For this app, pool needs to be unauthenticated pool with
     // AWS IoT permissions.
-    public static final String COGNITO_POOL_ID = "us-east-1:e7b4d445-36ed-4dfb-a40b-6fd6cb98f833";
+    public static final String COGNITO_POOL_ID = "us-east-1:XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX";
 
     // Region of AWS IoT
     public static final Regions MY_REGION = Regions.US_EAST_1;
